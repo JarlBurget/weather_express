b7f5ef24ba568affbca137c2426c59548a808dca
jarl <jarl-ivor.burget@voco.ee>
2024-12-04 12:48:28 +0200

    Create form request responce with API data

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 const key = 'd8c1d68bf61f928228c39b0632f16ed0'
 let city = 'Tartu'
 
@@ -30,6 +33,24 @@ app.get('/', (req, res) =>{
     })
 })
 
+app.post('/', (req,res) =>{
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) =>{
+        return response.json()
+    })
+    .then((data) =>{
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp)-273.15)
+        res.render('index',{
+            description: description,
+            city: city,
+            temp: temp
+        } )
+    } )
+})
+
 app.listen(3002, () => {
     console.log('http://localhost:3002')
-} )
+})
