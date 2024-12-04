456ad7eb0262a2d2a7b32d6f25a4b8deaefe8403
jarl <jarl-ivor.burget@voco.ee>
2024-12-04 12:29:53 +0200

    Add API data to project template file

--- index.js
+++ index.js
@@ -8,8 +8,26 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
 
+const key = 'd8c1d68bf61f928228c39b0632f16ed0'
+let city = 'Tartu'
+
 app.get('/', (req, res) =>{
-    res.render('index')
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    console.log(url)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) =>{
+        return response.json()
+    })
+    .then((data) =>{
+        let description = data.weather[0].description  
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp)-273.15)
+        res.render('index', {
+        description: description,
+        city: city,
+        temp: temp
+     })
+    })
 })
 
 app.listen(3002, () => {
