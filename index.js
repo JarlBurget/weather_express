dba16ab4f5f7b868c0aa5197b16c53a2ec841230
jarl <jarl-ivor.burget@voco.ee>
2024-12-04 13:09:44 +0200

    Refactor code dublicate with Promice for data fetching

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const exp = require('constants')
 const express = require('express')
 const path = require('path')
 
@@ -12,12 +13,11 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 const key = 'd8c1d68bf61f928228c39b0632f16ed0'
-let city = 'Tartu'
 
-app.get('/', (req, res) =>{
+const getWeatherData = (city) =>{
+    return new Promise((resolve, reject) =>{ 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-    console.log(url)
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    fetch(url)
     .then((response) =>{
         return response.json()
     })
@@ -25,31 +25,41 @@ app.get('/', (req, res) =>{
         let description = data.weather[0].description  
         let city = data.name
         let temp = Math.round(parseFloat(data.main.temp)-273.15)
-        res.render('index', {
+        const result = {
         description: description,
         city: city,
         temp: temp
-     })
+     }
+     resolve(result)
+    })
+    .catch(error =>{
+        reject(error)
+    } )
+})
+}
+
+app.get('/', (req,res) =>{
+    let city 
+    if(req.method == 'GET'){
+        city = 'Tartu'
+    } else if (req.method == 'POST'){
+        city = req.body.cityname
+    } 
+    getWeatherData(city)
+    .then((data) =>{
+        res.render('index', data)
     })
 })
 
-app.post('/', (req,res) =>{
+app.post('/', (req, res) =>{
     let city = req.body.cityname
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-    .then((response) =>{
-        return response.json()
-    })
+    getWeatherData(city)
     .then((data) =>{
-        let description = data.weather[0].description
-        let city = data.name
-        let temp = Math.round(parseFloat(data.main.temp)-273.15)
-        res.render('index',{
-            description: description,
-            city: city,
-            temp: temp
-        } )
-    } )
+        res.render('index', data)
+    })
 })
+
+    
 
 app.listen(3002, () => {
     console.log('http://localhost:3002')
